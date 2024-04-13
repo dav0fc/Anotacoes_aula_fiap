0c1d988de20f8bf542c5bb691afeff775c223283
David Gabriel Gomes Fernandes <davidbiel2013@gmail.com>
2024-04-12 21:15:16 -0300

    Aula Web Dev

--- WEB-DEVELOPMENT/script.js
+++ WEB-DEVELOPMENT/script.js
@@ -12,6 +12,66 @@ console.log(`Olá ${nome}, seja bem-vindo!`) */
 var idade = prompt("Escreva sua idade")
 var trab = prompt("Escreva onde voce trabalha")
 alert(`Olá ${nome}, voce tem ${idade} anos e trabalha na(o) ${trab}`) */
-var nome = prompt("Digite seu nome: ");
-var titulo = document.querySelector('h1')
-titulo.innerText = `Olá ${nome}, seja bem-vindo!`
+// var nome = prompt("Digite seu nome: ");
+// var titulo = document.querySelector('h1');
+// titulo.innerText = `Olá ${nome}, seja bem-vindo!`;
+
+//Resultado
+var resultado = document.getElementById('resultado')
+
+function somar(){
+    //Entrada
+    var n1 = parseFloat(document.getElementById('n1').value)
+    var n2 = parseFloat(document.getElementById('n2').value)
+
+    //Calculo
+    var soma = n1 + n2
+
+    //Saida
+    resultado.innerText = soma
+    console.log(soma)
+}
+
+function subt(){
+    //Entrada
+    var n1 = parseFloat(document.getElementById('n1').value)
+    var n2 = parseFloat(document.getElementById('n2').value)
+
+    //Calculo
+    var soma = n1 - n2
+
+    //Saida
+    resultado.innerText = soma
+    console.log(soma)
+}
+
+function mult(){
+    //Entrada
+    var n1 = parseFloat(document.getElementById('n1').value)
+    var n2 = parseFloat(document.getElementById('n2').value)
+
+    //Calculo
+    var soma = n1 * n2
+
+    //Saida
+    resultado.innerText = soma
+    console.log(soma)
+}
+
+function divi(){
+    //Entrada
+    var n1 = parseFloat(document.getElementById('n1').value)
+    var n2 = parseFloat(document.getElementById('n2').value)
+
+    //Calculo
+    if (n2 != 0){
+        var soma = n1 / n2
+        resultado.innerText = soma
+    }    
+    else{
+        resultado.innerText = "Não se divide por 0"
+    }
+    //Saida
+    
+    console.log(soma)
+}
