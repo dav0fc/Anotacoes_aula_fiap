82c2bf577ac4d80f39ae3965e07c66069af6d330
David Gabriel Gomes Fernandes <davidbiel2013@gmail.com>
2024-04-19 21:09:15 -0300

    Aula Web Dev Switch

--- WEB-DEVELOPMENT/script.js
+++ WEB-DEVELOPMENT/script.js
@@ -17,61 +17,106 @@ alert(`Olá ${nome}, voce tem ${idade} anos e trabalha na(o) ${trab}`) */
 // titulo.innerText = `Olá ${nome}, seja bem-vindo!`;
 
 //Resultado
-var resultado = document.getElementById('resultado')
+// var resultado = document.getElementById('resultado')
 
-function somar(){
-    //Entrada
-    var n1 = parseFloat(document.getElementById('n1').value)
-    var n2 = parseFloat(document.getElementById('n2').value)
+// function somar(){
+//     //Entrada
+//     var n1 = parseFloat(document.getElementById('n1').value)
+//     var n2 = parseFloat(document.getElementById('n2').value)
 
-    //Calculo
-    var soma = n1 + n2
+//     //Calculo
+//     var soma = n1 + n2
 
-    //Saida
-    resultado.innerText = soma
-    console.log(soma)
-}
+//     //Saida
+//     resultado.innerText = soma
+//     console.log(soma)
+// }
 
-function subt(){
-    //Entrada
-    var n1 = parseFloat(document.getElementById('n1').value)
-    var n2 = parseFloat(document.getElementById('n2').value)
+// function subt(){
+//     //Entrada
+//     var n1 = parseFloat(document.getElementById('n1').value)
+//     var n2 = parseFloat(document.getElementById('n2').value)
 
-    //Calculo
-    var soma = n1 - n2
+//     //Calculo
+//     var soma = n1 - n2
 
-    //Saida
-    resultado.innerText = soma
-    console.log(soma)
-}
+//     //Saida
+//     resultado.innerText = soma
+//     console.log(soma)
+// }
 
-function mult(){
-    //Entrada
-    var n1 = parseFloat(document.getElementById('n1').value)
-    var n2 = parseFloat(document.getElementById('n2').value)
+// function mult(){
+//     //Entrada
+//     var n1 = parseFloat(document.getElementById('n1').value)
+//     var n2 = parseFloat(document.getElementById('n2').value)
 
-    //Calculo
-    var soma = n1 * n2
+//     //Calculo
+//     var soma = n1 * n2
 
-    //Saida
-    resultado.innerText = soma
-    console.log(soma)
-}
+//     //Saida
+//     resultado.innerText = soma
+//     console.log(soma)
+// }
 
-function divi(){
-    //Entrada
-    var n1 = parseFloat(document.getElementById('n1').value)
-    var n2 = parseFloat(document.getElementById('n2').value)
+// function divi(){
+//     //Entrada
+//     var n1 = parseFloat(document.getElementById('n1').value)
+//     var n2 = parseFloat(document.getElementById('n2').value)
 
-    //Calculo
-    if (n2 != 0){
-        var soma = n1 / n2
-        resultado.innerText = soma
-    }    
-    else{
-        resultado.innerText = "Não se divide por 0"
-    }
-    //Saida
+//     //Calculo
+//     if (n2 != 0){
+//         var soma = n1 / n2
+//         resultado.innerText = soma
+//     }    
+//     else{
+//         resultado.innerText = "Não se divide por 0"
+//     }
+//     //Saida
     
-    console.log(soma)
+//     console.log(soma)
+// }
+// function calculo_livros(){
+//     var livros  = parseFloat(document.getElementById('livros').value)
+//     var calculo = 0
+//     if (livros < 7){
+//         var calculo = livros * 22.00
+//         console.log("1")
+//     }
+//     else{
+//         var calculo = livros * 15.00
+//         console.log("2")
+//     }
+//     resultado.innerText = `Irá ficar R$${calculo} reais totais`
+// }
+function escolha_cargo(){
+    var cargo  = document.getElementById('cargo').value
+    var calculo = 0
+    var salario = 3000
+    console.log(cargo)
+    switch (cargo){
+        case 'junior': 
+            calculo = salario 
+            console.log("1")
+            break
+        case 'pleno':
+            calculo = salario * 2
+            console.log("2")
+            break
+        case 'senior':
+            calculo = salario * 3
+            console.log("3")
+            break
+        case 'techlead':
+            calculo = salario * 4
+            console.log("4")
+            break
+        case 'diretor':
+            calculo = salario * 5
+            console.log("5")
+            break
+        default:
+            console.log("6")
+            break
+    }
+    cargo_print.innerText = `O salario será R$${calculo} totais`
 }
