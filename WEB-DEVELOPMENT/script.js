5008916b1249c25d53bfee00aa4cb81dd4dfe7e8
David Gabriel Gomes Fernandes <davidbiel2013@gmail.com>
2024-05-10 21:25:50 -0300

    aula Array

--- WEB-DEVELOPMENT/script.js
+++ WEB-DEVELOPMENT/script.js
@@ -163,24 +163,75 @@ alert(`Olá ${nome}, voce tem ${idade} anos e trabalha na(o) ${trab}`) */
 //     console.log(somar(1,2))
 // }
 // printarTela()
-function escolhaDoPc(){
-    const jokenpo = ["pedra","papel","tesoura"]
-    var numeroAleatorio = Math.floor(Math.random()*3)
-    return jokenpo[numeroAleatorio]
-}
-function escolhaJogador(escolha){
-    var escolhaPc = escolhaDoPc()
-    var resultado = ''
-    console.log(escolha)
-    console.log(escolhaPc)
-    if (escolha === escolhaPc){
-        resultado = 'Deu empate';
+// function escolhaDoPc(){
+//     const jokenpo = ["pedra","papel","tesoura"]
+//     var numeroAleatorio = Math.floor(Math.random()*3)
+//     return jokenpo[numeroAleatorio]
+// }
+// function escolhaJogador(escolha){
+//     var escolhaPc = escolhaDoPc()
+//     var resultado = ''
+//     console.log(escolha)
+//     console.log(escolhaPc)
+//     if (escolha === escolhaPc){
+//         resultado = 'Deu empate';
+//     }
+//     else if(escolha === 'pedra' && escolhaPc === 'tesoura' || escolha === 'papel' && escolhaPc === 'pedra' || escolha === 'tesoura' && escolhaPc === 'papel'){
+//         resultado = 'Voce ganhou';
+//     }
+//     else{
+//         resultado = 'Voce perdeu'
+//     }
+//     document.getElementById('resultado').innerHTML = resultado
+// }
+const lista = [2,4,6]
+// lista.push('Caio')
+// //adiciono no final do array
+
+// lista.pop()
+// //retira do array
+
+// lista.unshift('Julius')
+// //Add no começo do array
+
+// lista.shift()
+// //remove do começo do array
+
+// lista[1] = 'Denise'
+
+// console.log(lista)
+
+// //forEach
+// lista.forEach(function rodarArray(pessoa){
+//     console.log(`Ola ${pessoa}`)
+
+// })
+// lista.forEach(elemento =>console.log(`Ola ${elemento} `))
+
+
+// function dobrar(array = []){
+//     return array = array.map(element => element.toUpperCase())
+// }
+
+// console.log(dobrar(['oi','tudo bem', 'olar']))
+
+const filmes = [
+    {
+        nome: "Senhor dos Aneis",
+        diretor:"Peter Jackson",
+        ano:1977
+    },
+    {
+        nome: "Up",
+        diretor: "Pete Doctor",
+        ano: 2007
+    },
+    {
+        nome:"Homem-Aranha",
+        direto:"Marvel",
+        ano:2003
     }
-    else if(escolha === 'pedra' && escolhaPc === 'tesoura' || escolha === 'papel' && escolhaPc === 'pedra' || escolha === 'tesoura' && escolhaPc === 'papel'){
-        resultado = 'Voce ganhou';
-    }
-    else{
-        resultado = 'Voce perdeu'
-    }
-    document.getElementById('resultado').innerHTML = resultado
-}
+]
+
+const filtrado = filmes.filter(filmes => filmes.ano > 2000)
+console.log(filtrado)
